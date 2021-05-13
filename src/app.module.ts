d4888dd438cd657c1a69a4f0bf3ca125159c7c65
Yan Franca <yanguyt@gmail.com>
2021-05-13 11:10:08 -0300

    jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
-
+import {JwtModule} from "@nestjs/jwt"
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig),TasksModule],
+  imports: [
+    PassportModule.register({defaultStrategy: "jwt"}),
+    JwtModule.register({
+      secret: "testeSecret",
+      signOptions:{
+        expiresIn: 3600
+      }
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule
+  ],
 })
 export class AppModule {}
